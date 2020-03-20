dd0764fed0c1c9fd951f8c19f85eb433ba21c1a8
mohammed hindawash <djslakivich@gmail.com>
2020-03-20 13:45:10 -0400

    edited to try and deploy to gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,18 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :tetro, TetroWeb.Endpoint,
-http: [port: {:system, "PORT"}],
-url: [scheme: "https", host: "moestetris.herokuapp.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  load_from_system_env: true,
+  url: [host: "example.com", port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true,
+  secret_key_base: "${SECRET_KEY_BASE}"
+
+config :tetro, Tetro.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: "${DATABASE_URL}"
+  database: "",
+  ssl: true,
+  pool_size: 1
 
 # Do not print debug messages in production
 config :logger, level: :info
--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule Tetro.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.17.4"},
+      {:distillery, "~> 2.1", runetime: false},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_live_view, "~> 0.3.1"}
