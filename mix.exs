b857632f49bb90174356b71dc5a8d5eca7fe09b8
mohammed hindawash <djslakivich@gmail.com>
2020-03-24 18:49:34 -0400

    changed versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tetro.MixProject do
     [
       app: :tetro,
       version: "0.1.0",
-      elixir: "~> 1.5.3",
+      elixir: "~> 1.10.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
